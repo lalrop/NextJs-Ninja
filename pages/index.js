a80fd1862a578758acb0c8881680d4e32a4981e2
lalrop <lalrop@hotmail.com>
2022-02-22 17:47:54 -0300

    no es una buena opcion estar pegando el footer y navbar en cada una de las paginas. En reemplazo a esto creamos un componente llamado Layout donde generamos el esqueleto de la pagina donde señalamos los componentes que seran transversales a cada vista. Este componente lo introducimos en app con ambas etiquetas y dentro de este ubicamos el componente previamente instalado, luego este componente es referenciado a traves del argumento children en el componente Layout

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,12 @@
 import Link from 'next/link'
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Hello World!</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius consectetur rerum hic reiciendis nam pariatur laboriosam! Amet eius debitis deserunt esse natus error repellendus quaerat aperiam aspernatur eum. Incidunt, necessitatibus!</p>
       <Link href="ninjas/test"><a> Go to all Ninjas</a></Link>
-      <Footer />
     </div>
     )
 }
